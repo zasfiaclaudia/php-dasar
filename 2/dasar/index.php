34fd0e30c58a77a0c91977872d19f53a956a1152
zasfiaclaudia <112447707+zasfiaclaudia@users.noreply.github.com>
2022-12-04 21:46:08 +0700

    Add files via upload

---
+++ 2/dasar/index.php
@@ -0,0 +1,25 @@
+<?php 
+    echo "belajar";
+    echo "<br>";
+    echo 100;
+
+    echo "<br>";
+
+    $huruf = "Saya Belajar";
+
+    $angka = 100;
+
+        // echo $huruf;
+?>
+
+<?php 
+    echo '<h1>'.$huruf.'</h1>';
+    echo "<br>";
+    echo "<h2>$huruf</h2>";
+
+    echo "$angka";
+    echo "<br>";
+?>
+
+<p><?= $huruf ?></p>
+
