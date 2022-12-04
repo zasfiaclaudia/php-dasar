40e4191a924c2dbc16594ddc01a785eb6ad8ed4d
zasfiaclaudia <112447707+zasfiaclaudia@users.noreply.github.com>
2022-12-04 21:51:01 +0700

    Add files via upload

---
+++ 6/project/content.php
@@ -0,0 +1,14 @@
+<?php 
+
+    $titel = "Blog belajar PHP SMK - Coding itu mudah";
+
+    $logo = 'SMK-Revit';
+    $nav = [
+        'Home' => 'index.php',
+        'About'=>'isi-PHP/about.php',
+        'Produk' => 'produk.php',
+        'Peta' => 'peta.php',
+        'Contact'=>'contact.php',
+        'Blog'=>'isi-PHP/blog.php'];
+
+?>
---
+++ 6/project/isi-PHP/blog.php
@@ -0,0 +1,2 @@
+<h1>Blog</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, iure similique? Facilis, ipsa! Velit placeat cum voluptates praesentium, eveniet et cumque suscipit itaque iure quam hic, ut nobis optio nisi.</p>
